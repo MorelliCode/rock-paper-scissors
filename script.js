72bc36087190eb63f6b745f1b8c470a8cdf7dfb8
Pietro <pietro.morelli@gmail.com>
2023-02-19 19:05:04 -0300

    Create js script, test event listeners.

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+var rock = document.getElementById('rock');
+var paper = document.getElementById('paper');
+var scissors = document.getElementById('scissors');
+
+// Add event listeners to the buttons
+rock.addEventListener("click", function() {
+    console.log(rock);
+})
+
+paper.addEventListener("click", function() {
+    console.log(paper);
+})
+
+scissors.addEventListener("click", function() {
+    console.log(scissors);
+})
